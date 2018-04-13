5932c8040c8030715ff32fe9b6fc416c308eb039
Tomáš Witek <tomas@witek.cz>
2018-04-13 11:36:14 +0200

    Init commit

---
+++ cart/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "@panter/core";
+
+const index = () => {
+  return (
+    <div>
+      <Header />FUckl
+    </div>
+  );
+};
+
+export default index;
